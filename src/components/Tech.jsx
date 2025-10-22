54368715e71f0ec179e6af62eb76246db353cce6
Maryam Al-Naami <maryamalnaami@gmail.com>
2025-10-22 01:32:51 -0500

    Added projects section + project cards w/ react tilt

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -12,7 +12,7 @@ const Tech = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>What I have learned so far</p>
-        <h2 className={styles.sectionHeadText}>Technologies</h2>
+        <h2 className={styles.sectionHeadText}>Technologies.</h2>
       </motion.div>
 
       <div className="flex flex-row flex-wrap justify-center gap-10">
